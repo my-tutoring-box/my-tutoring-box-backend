21d45e0a8b5ebd523b966029d95246a0df916e63
JIGOOOD <helmrping@gmail.com>
2025-03-16 03:36:58 +0900

    chore: set initial folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { StudentsModule } from './students/students.module';
+import { LessonsModule } from './lessons/lessons.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, StudentsModule, LessonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
