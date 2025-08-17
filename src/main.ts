997b4dc62a45fd33b8d4e8fe9afbc621a8cbe1a4
JIGOOOD <helmrping@gmail.com>
2025-08-18 00:55:17 +0900

    chore: add public ip to access frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'http://54.252.225.141:3001'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
